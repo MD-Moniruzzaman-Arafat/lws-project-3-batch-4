30ae567c956be1113cce2f29f484f0cd6d232708
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-11-24 16:45:23 +0600

    header component design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Header from './components/Header/Header';
 import SideBar from './components/SideBar/SideBar';
 
 function App() {
@@ -7,6 +8,11 @@ function App() {
       <div className="min-h-screen flex flex-col lg:flex-row">
         {/* Sidebar */}
         <SideBar />
+        {/* Main Content */}
+        <main className="flex-1 flex flex-col min-h-0">
+          {/* Header */}
+          <Header />
+        </main>
       </div>
     </>
   );
